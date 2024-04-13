e2b8a0a9f0eb68870725eb9f4d88f0d0457d4e23
kkawamura <kengokawamura0@gmail.com>
2024-04-13 13:29:39 +0900

    add header

---
+++ my-closet-app/src/app/components/navbar.js
@@ -0,0 +1,11 @@
+export default function navbar() {
+    return (
+        <nav class="nav">
+            <ul>
+                <li><a href="./index.html">WebSite</a></li>
+                <li><a href="./index.html">TOP</a></li>
+                <li><a href="./contact.html">CONTACT</a></li>
+            </ul>
+        </nav>
+    )
+}
