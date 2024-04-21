6fa51d3e72f733079ccf3ee143b3f5c93a166ed6
kkawamura <kengokawamura0@gmail.com>
2024-04-21 17:53:05 +0900

    fontとか

--- my-closet-app/src/app/components/navbar.js
+++ my-closet-app/src/app/components/navbar.js
@@ -1,11 +1,14 @@
+import navbar_style from './navbar.module.css'
+import { notojp } from '../utils/font'
+
 export default function navbar() {
     return (
-        <nav class="nav">
-            <ul>
-                <li><a href="./index.html">WebSite</a></li>
-                <li><a href="./index.html">TOP</a></li>
-                <li><a href="./contact.html">CONTACT</a></li>
-            </ul>
-        </nav>
+        <nav className={notojp.className}>
+            < h1 className={navbar_style.ul} >
+                <li className={navbar_style.li}><a href="/">トップ</a></li>
+                <li className={navbar_style.li}><a href="/category">クローゼット一覧</a></li>
+                <li className={navbar_style.li}><a href="./index.html">設定</a></li>
+            </h1 >
+        </nav >
     )
 }
